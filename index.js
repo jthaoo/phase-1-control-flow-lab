876b61016916ed1dde60b9e0200d0c3364416d5c
Johnathan Thao <johnathanthao21@gmail.com>
2022-06-14 09:22:01 -0600

    Done with assignment

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400) return "This one is on me!";
+  else if (feet >= 400 && feet < 2000) return "That will be twenty bucks.";
+  else if (feet < 2500 && feet > 2000) return "I will gladly take your thirty bucks.";
+  else if (feet > 2500) return "No can do.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  const cityMessage = (city === 'NYC') ? ('Ok, sounds good.') : ('No go.');
+  return cityMessage;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+      case 'generous':
+        return 'Thank you so much.';
+        break;
+      case 'not as generous':
+        return 'Thank you.';
+        default: return 'Bye.'
 }
+      }
